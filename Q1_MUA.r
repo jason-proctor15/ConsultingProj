18e518f95de8c529a8126713c3e2c65dc04cab1b
jason-proctor15 <112121629+jason-proctor15@users.noreply.github.com>
2022-10-07 14:25:20 -0500

    Update Q1_MUA.r

--- Q1_MUA.r
+++ Q1_MUA.r
@@ -26,3 +26,59 @@ ggbarstats(
 
 
 # reference : https://statsandr.com/blog/fisher-s-exact-test-in-r-independence-test-for-a-small-sample/
+
+##### Jasons code
+#Simulating how many No mua patients are needed to reject null
+#Loop creating p-values from fisher exact test for n= 1:500
+n = 500
+pvalz = c()
+for (i in 1:n) {
+  x = matrix(c(3, 9, 21, i), ncol = 2)
+  pvalz[i] = fisher.test(x)$p.value
+}
+
+
+#Plot of p-values with critical p=0.05 abline
+plot(c(seq(1, 500, 1)), 
+     pvalz, pch = 16, 
+     main = "Number of MUA free patients required to reject null",
+     xlab = "n", ylab= "P-value")
+abline(h = 0.05, col = "red")
+#n=293 is the first number that passes the critical value
+
+
+
+
+##############
+#Potential Visualizations of data
+#Full patient lists
+library(readxl)
+Pts_full = read_excel("C:/Users/grfri/Google Drive/Spring 2022/Design/Q1.xlsx", sheet = "OriginalData")
+Pts_2TKA = read_excel("C:/Users/grfri/Google Drive/Spring 2022/Design/Q1.xlsx", sheet = "2TKA")
+
+#Separated patients with only 1 TKA into different group
+Pts_1TKA =read_excel("C:/Users/grfri/Google Drive/Spring 2022/Design/Q1.xlsx", sheet = "1TKA")
+
+
+#Histogram of MUA count on one knee
+hist(Pts_1TKA$mua_count, xlab = "Frequency of multiple MUAs on one knee")
+
+
+#Comparing health of 1 TKA patients vs 2 TKA patients
+#Histogram of ASA rating 
+par(mfrow=c(1,2))
+hist(Pts_1TKA$asa_rating, main="Patients with 1 TKA", breaks = 5, xlim = range(0:4), xlab = "ASA Rating")
+hist(Pts_2TKA$asa_rating, main = "Patients with 2 TKA", breaks = 5, xlim = range(0:4), xlab = "ASA Rating")
+#While the health of each group appears similar, patients with 2 TKA's appear more right skewed
+#Good to keep in mind that patients with multiple TKAs may be more unhealthy and that needs to be controlled for.
+
+
+#Correlation Matrix of comorbities
+ #Comorbities without "platelet_transfusion", "AIDS", "wound_infection", "hematoma", "knee_infection"
+#These excluded variables had no counts
+library(ggcorrplot)
+comorb_2 = Pts_full[,c(20, 24:38)] 
+ggcorrplot(cor(comorb_2), title = "Correlation matrix of comorbities of patients who underwent MUA")
+
+
+
